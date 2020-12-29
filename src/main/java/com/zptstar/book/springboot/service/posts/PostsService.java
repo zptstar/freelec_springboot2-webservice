3ff985595dca86e81ad4d75e306da6855b1b2d63
zptstar <zptstar2@hotmail.com>
2020-12-29 23:16:07 +0900

    JPA test 1. Post_저장, 조회 구현

--- src/main/java/com/zptstar/book/springboot/service/posts/PostsService.java
+++ src/main/java/com/zptstar/book/springboot/service/posts/PostsService.java
@@ -1,10 +1,17 @@
 package com.zptstar.book.springboot.service.posts;
 
+import com.zptstar.book.springboot.domain.posts.Posts;
 import com.zptstar.book.springboot.domain.posts.PostsRepository;
+import com.zptstar.book.springboot.web.dto.PostsResponseDto;
 import com.zptstar.book.springboot.web.dto.PostsSaveRequestDto;
+import com.zptstar.book.springboot.web.dto.PostsUpdateRequestDto;
+import com.zptstar.book.springboot.web.dto.PostsListResponseDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Service
@@ -14,5 +21,29 @@ public class PostsService {
     @Transactional
     public Long save(PostsSaveRequestDto requestDto) {
         return postsRepository.save(requestDto.toEntity()).getId();
+    }
+
+    @Transactional
+    public Long update(Long id, PostsUpdateRequestDto requestDto) {
+        Posts posts = postsRepository.findById(id)
+                                    .orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id="+ id));
+
+        posts.update(requestDto.getTitle(), requestDto.getContent());
+
+        return id;
+    }
+
+    public PostsResponseDto findById (Long id) {
+        Posts entity = postsRepository.findById(id)
+                                    .orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id="+ id));
+
+        return new PostsResponseDto(entity);
+    }
+
+    @Transactional(readOnly = true)
+    public List<PostsListResponseDto> findAllDesc() {
+        return postsRepository.findAllDesc().stream()
+                .map(PostsListResponseDto::new)
+                .collect(Collectors.toList());
     }
 }
---
+++ src/main/java/com/zptstar/book/springboot/web/IndexController.java
@@ -0,0 +1,32 @@
+package com.zptstar.book.springboot.web;
+
+import com.zptstar.book.springboot.service.posts.PostsService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@RequiredArgsConstructor
+@Controller
+public class IndexController {
+
+    private final PostsService postsService;
+
+//    @GetMapping("/")
+//    public String index() {
+//        return "index";
+//    }
+
+    @GetMapping("/")
+    public String index(Model model) {
+        model.addAttribute("posts", postsService.findAllDesc());
+
+        return "index";
+    }
+
+    @GetMapping("/posts/save")
+    public String postsSave() {
+        return "posts-save";
+    }
+
+}
