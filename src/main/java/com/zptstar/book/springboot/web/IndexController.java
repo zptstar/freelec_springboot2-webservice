571bc1cf8d907622070aa0f223ad8d3b163557b9
zptstar <zptstar2@hotmail.com>
2020-12-30 23:15:58 +0900

    OAuth google login

--- src/main/java/com/zptstar/book/springboot/web/IndexController.java
+++ src/main/java/com/zptstar/book/springboot/web/IndexController.java
@@ -1,5 +1,6 @@
 package com.zptstar.book.springboot.web;
 
+import com.zptstar.book.springboot.config.auth.dto.SessionUser;
 import com.zptstar.book.springboot.service.posts.PostsService;
 import com.zptstar.book.springboot.web.dto.PostsResponseDto;
 import lombok.RequiredArgsConstructor;
@@ -8,11 +9,14 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import javax.servlet.http.HttpSession;
+
 @RequiredArgsConstructor
 @Controller
 public class IndexController {
 
     private final PostsService postsService;
+    private final HttpSession httpsSession;
 
 //    @GetMapping("/")
 //    public String index() {
@@ -22,6 +26,12 @@ public class IndexController {
     @GetMapping("/")
     public String index(Model model) {
         model.addAttribute("posts", postsService.findAllDesc());
+
+        SessionUser user = (SessionUser) httpsSession.getAttribute("user");
+
+        if (user != null) {
+            model.addAttribute("userName", user.getName());
+        }
 
         return "index";
     }
